67e9b62f60da877e912a698c10b62bb9205941b0
Andres Carvajal <jc1993box@hotmail.com>
2020-02-16 17:42:30 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,42 @@
+require 'pry'
+class Song 
+  
+attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  
+def initialize(song_name, artist, genre)
+  @name=song_name
+  @artist=artist
+  @genre=genre
+  @@count+=1 
+  @@artists << artist 
+  @@genres << genre 
+end
+
+def self.count 
+  @@count
+end 
+
+def self.artists
+  @@artists.uniq
+end  
+
+def self.genres
+  @@genres.uniq
+end
+
+def self.genre_count
+  genres = {}
+  @@genres.each do |genre|
+    if genre_count[genre]
+      genre_count [genre]+= 1 
+    else 
+      genre[count] = 1 
+    end
+    genre_count
+  end 
+end
+
+end
